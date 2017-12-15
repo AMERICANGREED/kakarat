552a7350713d5a67afaebcbf0655cefbaf17df16
dcoreyrasm <dcorey.rasmussen@gmail.com>
2017-12-15 11:17:00 -0500

    changed connection
    changed connection

--- opendb.php
+++ opendb.php
@@ -1,12 +1,7 @@
 <?php
 // Create connection
-// $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 //
-$conn=mysqli_init();
-
-mysqli_ssl_set($conn, NULL, NULL, {ca-cert filename}, NULL, NULL);
-
-mysqli_real_connect($conn, "kakarat2-mysqldbserver.mysql.database.azure.com", "mysqldbuser@kakarat2-mysqldbserver", "Etinge2017", 'mysqldatabase45775', 3306);
 
 // Check connection
 if (!$conn) {
