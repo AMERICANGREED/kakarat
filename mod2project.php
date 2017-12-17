382f571e0bad5d574a32dd7bb0d40ed0c1d9f8e4
Etinge <leny0004@gmail.com>
2017-12-17 08:28:06 -0600

    1

--- mod2project.php
+++ mod2project.php
@@ -32,7 +32,7 @@
 					if (mysqli_num_rows($result) > 0) {
 					    // output data of each row
 					    while($row = mysqli_fetch_assoc($result)) {
-									echo "Word of the day is : " . $row["wordoftheday"]. ;
+									echo "Word of the day is : " . $row["wordoftheday"]. "<br>";
 					        
 					    }
 					} else {
