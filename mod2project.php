6b1a6e2551f0f5e4f4f1b12d11aae371bfff57ad
Etinge <leny0004@gmail.com>
2017-12-17 08:57:21 -0600

    1

--- mod2project.php
+++ mod2project.php
@@ -1,20 +1,19 @@
 <html>
 		<head>
-	<title>Get Word of the day </title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Employee Search</title>
+	<!-- <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="styles/custom.css" />
 <link rel="stylesheet" href="themes/rasmussenthemeroller.min.css" />
 <link rel="stylesheet" href="themes/jquery.mobile.icons.min.css" />
 <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile.structure-1.4.5.min.css" />
 <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
-<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
-<script src="javascript/storage.js"></script>
+<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script> -->
 </head>
 	<body>
 		<div id="page" data-role="page" data-theme="b" >
 	<div data-role="header" data-theme="b">
 <h1>
-	Word Game 
+	Employee Search
 		</h1>	</div>
 				<div data-role="content">
 
@@ -23,33 +22,32 @@
 					include 'config.php';
 					include 'opendb.php';
 
-					$number = (isset($_POST['number'])    ? $_POST['number']   : '');
+					
 
-					$sql= "SELECT number FROM wordgame
-					WHERE number = '$number' LIMIT 10";
+					$firstname = (isset($_POST['firstname'])    ? $_POST['firstname']   : '');
+					$lastname = (isset($_POST['lastname'])    ? $_POST['lastname']   : '');
+
+					$sql= "SELECT firstname, lastname
+					FROM employees
+					WHERE firstname = '$firstname' OR lastname = '$lastname' LIMIT 10";
+
 					$result = mysqli_query($conn, $sql);
-
-					if (mysqli_num_rows($result) > 0) 
-					{
+					if (mysqli_num_rows($result) > 0) {
 					    // output data of each row
 					    while($row = mysqli_fetch_assoc($result)) {
-									echo "Word of the day is : " . $row["wordoftheday"]. "<br><hr>";
 									
-					        
+					        echo "First Name: " . $row["firstname"]. "<br>";
+					        echo "Last Name: " . $row["lastname"]. "<br><hr>";
 					    }
-					}
-					else 
-					
-					{
+					} else {
 					    echo "0 results";
 					}
-
 					mysqli_close($conn);
-
 					?>
+				</div>
 
 				<div data-role="footer" data-theme="b">
-	  <h4>elenyaapp &copy; 2017</h4>
-	</div>
+	  <h4> elenyaapp </h4>
+	</div> 	</div>
 	</body>
 </html>
