e1c563f09502f115c76392e3f1c9d30b7e31c22a
Etinge <leny0004@gmail.com>
2017-12-17 20:50:35 -0600

    1

--- activity3.php
+++ activity3.php
@@ -33,7 +33,7 @@
 					if (mysqli_num_rows($result) > 0) {
 					    // output data of each row
 					    while($row = mysqli_fetch_assoc($result)) {
-									echo "ID: " . $row["id"]. "<br>";
+									echo "email: " . $row["email"]. "<br>";
 					        echo "First Name: " . $row["fname"]. "<br>";
 					        echo "Last Name: " . $row["lname"]. "<br>";
 									echo "Phone #: " . $row["phone"]. "<br>";
